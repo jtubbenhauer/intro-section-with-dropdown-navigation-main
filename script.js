eb106ad324c8d987d70fc702f621a1c1ac9d3c36
Jack <jack@nimbusvapour.com.au>
2022-06-08 12:19:36 +1000

    mobile menu transition

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("works");
+const openMenuHandler = () => {
+  const menu = document.getElementById("mobile-menu");
+  const shadow = document.getElementById("shadow");
+  shadow.style.opacity = 0.8;
+  shadow.style.visibility = "visible";
+  menu.style.visibility = "visible";
+  menu.style.opacity = 1;
+};
+
+const closeMenuHandler = () => {
+  const menu = document.getElementById("mobile-menu");
+  const shadow = document.getElementById("shadow");
+  menu.style.visibility = "hidden";
+  menu.style.opacity = 0;
+  shadow.style.visibility = "hidden";
+  shadow.style.opacity = 0;
+};
